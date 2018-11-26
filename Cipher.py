14d0f1a9dd45c29e865e655f775bda4d384b1ca2
JonahSpicher <jonah.spicher@gmail.com>
2018-11-26 15:08:49 -0500

    Made some edits

--- Cipher.py
+++ Cipher.py
@@ -1,47 +1,33 @@
-"""Synthesizes a blues solo algorithmically."""
-
-import atexit
-import os
-from random import choice
-
 from psonic import *
-
-# The sample directory is relative to this source file's directory.
-SAMPLES_DIR = os.path.join(os.path.dirname(__file__), "samples")
-
-SAMPLE_FILE = os.path.join(SAMPLES_DIR, "bass_D2.wav")
-SAMPLE_NOTE = D2  # the sample file plays at this pitch
+import numpy as np
+import string
 
 
-def play_note(note, beats=1, bpm=60, amp=1):
-    """Play note for `beats` beats. Return when done."""
-    # `note` is this many half-steps higher than the sampled note
-    half_steps = note - SAMPLE_NOTE
-    # An octave higher is twice the frequency. There are twelve half-steps per
-    # octave. Ergo, each half step is a twelth root of 2 (in equal temperament).
-    rate = (2 ** (1 / 12)) ** half_steps
-    assert os.path.exists(SAMPLE_FILE)
-    # Turn sample into an absolute path, since Sonic Pi is executing from a
-    # different working directory.
-    sample(os.path.realpath(SAMPLE_FILE), rate=rate, amp=amp)
-    sleep(beats * 60 / bpm)
+# Makes amplitude dictionary
+min = 1
+max = 26
+AMP_DICT = {}
+alphabet = string.ascii_lowercase
+amps = np.linspace(min, max, 26)
+for i in range(len(alphabet)):
+    AMP_DICT[alphabet[i]]=amps[i]
+
+def freq_to_midi(freq):
+    return 69 + 12 * np.log2(freq/440)
 
 
-def stop():
-    """Stop all tracks."""
-    msg = osc_message_builder.OscMessageBuilder(address='/stop-all-jobs')
-    msg.add_arg('SONIC_PI_PYTHON')
-    msg = msg.build()
-    synth_server.client.send(msg)
+def message_to_sound():
+    freqs = np.linspace(100, 700, 6)
+    message = input("Please Enter Something, please no more than six letters for now\n")
+    if len(message)>6:
+        print("Fuck you.")
+    else:
+        if len(message) < 6:
+            message = message + '0'*(6-len(message))
+        print(message)
+        # for i in range(len(freqs)):
+        #     play(freq_to_midi):
 
 
-# stop all tracks when the program exits normally or is interrupted
-atexit.register(stop)
-
-# These are the piano key numbers for a 3-octave blues scale in A.
-# See: http://en.wikipedia.org/wiki/Blues_scale
-BLUES_SCALE = [
-    40, 43, 45, 46, 47, 50, 52, 55, 57, 58, 59, 62, 64, 67, 69, 70, 71, 74, 76]
-BEATS_PER_MINUTE = 45				# Let's make a slow blues solo
-
-play_note(70, beats=3, bpm=BEATS_PER_MINUTE)
+if __name__ == "__main__":
+    message_to_sound()
